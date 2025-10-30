032a4fa4390079c4d96b31dde0e301b59125168b
jluis0007 <jluis.0007@hoatmail.com>
2025-10-30 15:29:35 -0600

    182. Componentes de búsqueda

--- src/heroes/pages/search/SearchPage.tsx
+++ src/heroes/pages/search/SearchPage.tsx
@@ -1,5 +1,6 @@
 import { CustomJumbotron } from "@/components/custom/CustomJumbotron";
 import { HeroStats } from "@/heroes/components/HeroStats";
+import { SearchControls } from "./ui/SearchControls";
 
 export const SearchPage = () => {
   return (
@@ -9,8 +10,12 @@ export const SearchPage = () => {
         title="Búsqueda de SuperHéroes"
         description="Descubre, explora y administra super héroes y villanos"
       />
+
       {/* Stats Dashboard */}
       <HeroStats />
+
+      {/* Filter and search */}
+      <SearchControls />
     </>
   );
 };
