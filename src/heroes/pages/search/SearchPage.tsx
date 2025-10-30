33c978b0552f0cf6e1b5087c5cba98d4948907e4
jluis0007 <jluis.0007@hoatmail.com>
2025-10-30 15:13:14 -0600

    181. Parte 2 - Pensemos en componentes

--- src/heroes/pages/search/SearchPage.tsx
+++ src/heroes/pages/search/SearchPage.tsx
@@ -1,4 +1,5 @@
 import { CustomJumbotron } from "@/components/custom/CustomJumbotron";
+import { HeroStats } from "@/heroes/components/HeroStats";
 
 export const SearchPage = () => {
   return (
@@ -8,6 +9,8 @@ export const SearchPage = () => {
         title="Búsqueda de SuperHéroes"
         description="Descubre, explora y administra super héroes y villanos"
       />
+      {/* Stats Dashboard */}
+      <HeroStats />
     </>
   );
 };
