3a42b2b5810980e874b9a0f564a38e1600ece8e5
jluis0007 <jluis.0007@hoatmail.com>
2025-11-27 13:15:48 -0600

    210. Contexto de favoritos - Parte 2

--- src/heroes/context/FavoriteHeroContext.tsx
+++ src/heroes/context/FavoriteHeroContext.tsx
@@ -1,38 +1,62 @@
-import { createContext, useState, type PropsWithChildren } from "react";
+import {
+  createContext,
+  useEffect,
+  useState,
+  type PropsWithChildren,
+} from "react";
 import type { Hero } from "../types/hero.interface";
+import { useFetcher } from "react-router";
 
 interface FavoriteHeroContext {
   // State
   favorites: Hero[];
   favoriteCount: number;
+
   //Methods
   isFavorite: (hero: Hero) => boolean;
   toggleFavorite: (hero: Hero) => void;
 }
 
+// eslint-disable-next-line react-refresh/only-export-components
 export const FavoriteHeroContext = createContext({} as FavoriteHeroContext);
 
+const getFavoritesFromlocalStorage = () => {
+  const favorites = localStorage.getItem("favorite");
+  return favorites ? JSON.parse("favorites") : [];
+};
+
 export const FavoriteHeroProvider = ({ children }: PropsWithChildren) => {
-  const [favorites, setFavorites] = useState<Hero[]>([]);
+  const [favorites, setFavorites] = useState<Hero[]>(
+    getFavoritesFromlocalStorage()
+  );
 
   const toggleFavorite = (hero: Hero) => {
     const heroExist = favorites.find((h) => h.id === hero.id);
+
     if (heroExist) {
       const newFavorites = favorites.filter((h) => h.id !== hero.id);
       setFavorites(newFavorites);
       return;
     }
+
     setFavorites([...favorites, hero]);
   };
+  const isFavorite = (hero: Hero) => {
+    return favorites.some((h) => h.id === hero.id);
+  };
+
+  useEffect(() => {
+    localStorage.setItem("favorites", JSON.stringify(favorites));
+  }, [favorites]);
 
   return (
     <FavoriteHeroContext
       value={{
         // State
-        favoriteCount: 0,
-        favorites: [],
+        favoriteCount: favorites.length,
+        favorites: favorites,
         // Methods
-        isFavorite: () => {},
+        isFavorite: isFavorite,
         toggleFavorite: toggleFavorite,
       }}
     >
